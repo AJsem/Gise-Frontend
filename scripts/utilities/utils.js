e417597fb5752a00ab6391a35f56d7bea509a113
Jxgrim <emmanuelavwomakpa@gmail.com>
2026-05-18 15:01:57 +0100

    dev 3.1.6

--- scripts/utilities/utils.js
+++ scripts/utilities/utils.js
@@ -1,8 +1,21 @@
+const scrolledNav = () => {
+    const navBar = document.querySelector("nav");
+
+    window.addEventListener("scroll", () => {
+        if(window.scrollY > 100)
+            navBar.classList.add("scrolled");
+        else
+            navBar.classList.remove("scrolled");
+    })
+}
+
 const navigation = () => {
+    scrolledNav();
     const navBtn = document.querySelector('nav .nav-btn');
     const dropDown = document.querySelector('nav .links');
 
     navBtn.addEventListener('click', () => {
+        // if(navBtn.classList.contains)
         navBtn.classList.toggle('active');
         dropDown.classList.toggle('active');
     });
